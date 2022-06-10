9056487418050d26302ffb0d1e404dc7cc62ba35
Fett00 <musaew2014@gmail.com>
2022-06-10 20:57:00 +0300

    Добавлена ячейка коллекции для списка заданий. Добавлена коллекция для списка заданий

--- HelpNear/Screens/EventCollection/EventCollectionCell.swift
+++ HelpNear/Screens/EventCollection/EventCollectionCell.swift
@@ -10,5 +10,63 @@ import UIKit
 
 class EventCollectionCell: UICollectionViewCell {
     
+    private let title: UILabel = {
+        
+        let label = UILabel()
+        
+        return label
+    }()
     
+    private let shortDescription: UILabel = {
+        
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    private let ageLimit: UIImageView = {
+        
+        let image = UIImageView()
+        
+        return image
+    }()
+    
+    private let goToMapButton: UIButton = {
+       
+        let button = UIButton()
+        
+        return button
+    }()
+    
+    private let acceptButton: UIButton = {
+        
+        let button = UIButton()
+        
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        confCell()
+        confSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func confCell(){
+        
+    }
+    
+    private func confSubviews(){
+        
+        self.addSubview(title, shortDescription, ageLimit, acceptButton)
+        
+    }
+    
+    func render(){
+        
+    }
 }
--- HelpNear/Screens/EventCollection/EventCollectionScreen.swift
+++ HelpNear/Screens/EventCollection/EventCollectionScreen.swift
@@ -6,6 +6,7 @@ import UIKit
 
 final class EventCollectionScreen: UIViewController {
     
+    //Коллекция с заданями
     private let collection: UICollectionView = {
        
         let layout = UICollectionViewFlowLayout()
@@ -14,6 +15,7 @@ final class EventCollectionScreen: UIViewController {
         return collection
     }()
     
+    //Строка с поиском
     private let searchBar: EventCollectionHeader = {
        
         let header = EventCollectionHeader()
@@ -28,12 +30,56 @@ final class EventCollectionScreen: UIViewController {
         confSubviews()
     }
     
+    //Обноовление лайаута коллекции
+    private func layoutCollection(){
+        
+        guard let flowLayout = collection.collectionViewLayout as? UICollectionViewFlowLayout else { return }
+        
+        let insets = UIEdgeInsets(top: 20, left: 20, bottom: 0, right: 20)
+        let spacing = 20.0
+        let deviceWidth = view.frame.width
+        let itemSize = (deviceWidth - insets.left - insets.right - spacing ) / 2
+        flowLayout.minimumInteritemSpacing = spacing
+        flowLayout.minimumLineSpacing = spacing
+        flowLayout.sectionInset = insets
+        flowLayout.itemSize = CGSize(width: itemSize, height: itemSize * 2)
+    }
+    
+    //configurate view
     private func confView(){
         
         self.view.backgroundColor = .systemBackground
     }
     
+    //configurate subviews
     private func confSubviews(){
         
+        
+        
+        collection.delegate = self
+        collection.dataSource = self
+        collection.register(EventCollectionCell.self, forCellWithReuseIdentifier: EventCollectionCell.reuseID)
+        
+        let safeArea = view.safeAreaLayoutGuide
+        
+        searchBar.constraints(top: safeArea.topAnchor, bottom: nil, leading: safeArea.leadingAnchor, trailing: safeArea.trailingAnchor, paddingTop: 0, paddingBottom: 0, paddingLeft: 0, paddingRight: 0, width: 0, height: 50)
+        collection.constraints(top: searchBar.bottomAnchor, bottom: safeArea.bottomAnchor, leading: safeArea.leadingAnchor, trailing: safeArea.trailingAnchor, paddingTop: 0, paddingBottom: 0, paddingLeft: 0, paddingRight: 0, width: 0, height: 0)
     }
+}
+
+extension EventCollectionScreen: UICollectionViewDataSource{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        UICollectionViewCell()
+    }
+    
+    
+}
+
+extension EventCollectionScreen: UICollectionViewDelegate{
+    
 }
