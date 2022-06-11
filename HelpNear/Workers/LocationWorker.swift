e76f6c3851ca94af7224ead04ea1e780299808d7
Fett00 <musaew2014@gmail.com>
2022-06-11 17:04:40 +0300

    Добавлено требование к получению локации

--- HelpNear/Workers/LocationWorker.swift
+++ HelpNear/Workers/LocationWorker.swift
@@ -7,9 +7,30 @@ import CoreLocation
 
 protocol LocationWorkerProtocol{
     
+    func requestPermissionIfDisabled()
 }
 
 //Работа с данными геопозиции
-final class LocationWorker{
+final class LocationWorker: NSObject, LocationWorkerProtocol{
     
+    private lazy var locationManager: CLLocationManager = {
+       
+        let manager = CLLocationManager()
+        manager.delegate = self
+        
+        return manager
+    }()
+    
+    func requestPermissionIfDisabled() {
+        
+        self.locationManager.requestAlwaysAuthorization()
+    }
+}
+
+extension LocationWorker: CLLocationManagerDelegate{
+
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        
+        print("LOCATION")
+    }
 }
